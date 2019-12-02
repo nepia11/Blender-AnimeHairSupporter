061c869b228d0ebc68ee526a5ccee15ccba33b51
Taremin <taremin.vr@gmail.com>
2019-12-03 07:57:15 +0900

    release 1.0.2

--- __init__.py
+++ __init__.py
@@ -7,8 +7,8 @@ import bpy
 # アドオン情報
 bl_info = {
     'name': "Anime Hair Supporter",
-    'author': "saidenka",
-    'version': (1, 0, 1),
+    'author': "saidenka, Taremin",
+    'version': (1, 0, 2),
     'blender': (2, 80, 0),
     'location': "3Dビュー > オブジェクトモード > サイドバー > ツール > アニメ髪支援パネル",
     'description': "",
